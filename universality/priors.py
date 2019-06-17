96e691547d029e09ac89713c71a468580a31d2f1
Reed Essick <reed.essick@ligo.org>
2019-06-17 16:58:19 -0500

    started a script to add in prior weights to a set of posterior samples so that we can properly account for them within KDEs meant to model the likelihood. NOTE: this is **not** complete, as I have to actually implement the priors and write the logic to generate the weights, writing the results to disk.

---
+++ universality/priors.py
@@ -0,0 +1,12 @@
+__doc__ = "a module for generating prior weights based on known sampling priors within lalinference"
+__author__ = "reed.essick@ligo.org"
+
+#-------------------------------------------------
+
+import numpy as np
+
+#-------------------------------------------------
+
+KNOWN_PRIORS = []
+
+#-------------------------------------------------
