3f6419e3e405f3398bd631fcfdd757dbb776ae7c
Reed Essick <reed.essick@ligo.org>
2019-06-17 17:07:36 -0500

    more work to get prior weights from lalinference sampling into production

--- universality/priors.py
+++ universality/priors.py
@@ -7,6 +7,22 @@ import numpy as np
 
 #-------------------------------------------------
 
-KNOWN_PRIORS = []
+KNOWN_PRIORS = [
+    'uniform',
+]
 
 #-------------------------------------------------
+
+def logprior(prior_type, data):
+    """compute the prior weight associated with each sample in data. This is a general routing function that should delegate to different types of priors, as specified in KNOWN_PRIORS"""
+    assert prior_type in KNOWN_PRIORS, 'prior=%s not understood! must be one of %s'%(prior_type, ', '.join(KNOWN_PRIORS))
+
+    if prior_type=='uniform':
+        return uniform(data)
+
+#-------------------------------------------------
+
+def uniform(data):
+    """return a flat prior regardless of the data (ie, just zeros of the correct length
+    """
+    return np.zeros(len(data), dtype=float)
