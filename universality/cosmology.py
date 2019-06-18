91cf01c34babf5a3e4aedbee6d30a1b62b9edb81
Reed Essick <reed.essick@ligo.org>
2019-06-18 13:07:08 -0500

    implemented the cosmological stuff I need

--- universality/cosmology.py
+++ universality/cosmology.py
@@ -11,6 +11,10 @@ from . import utils
 
 #-------------------------------------------------
 
+DEFAULT_DZ = 1e-2 ### should be good enough for most things we want to do
+
+#-------------------------------------------------
+
 class Cosmology(object):
     """a class that implements specific cosmological computations.
 **NOTE**, we work in CGS units throughout, so Ho must be specified in s**-1
@@ -28,26 +32,73 @@ class Cosmology(object):
 
         assert self.OmegaKappa==0, 'we only implement flat cosmologies! OmegaKappa must be 0'
 
+        self._init_memo() ### instantiate the memorized interpolation arrays
+
+    def _init_memo(self):
+        """instantiate things to "memorize" results and cache them
+        """
+        self._distances = {'z':np.array([0]), 'DL':np.array([0])}
+
     def z2E(self, z):
         """returns E(z) = sqrt(OmegaLambda + OmegaKappa*(1+z)**2 + OmegaMatter*(1+z)**3 + OmegaRadiation*(1+z)**4)
         """
         one_plus_z = 1+z
         return (self.OmegaLambda + self.OmegaKappa*one_plus_z**2 + self.OmegaMatter*one_plus_z**3 + self.OmegaRadiation*one_plus_z**4)**0.5
 
-    def DL2z(self, DL):
+    def dDLdz(self, z):
+        """returns (c/Ho)/E(z)
         """
-        returns redshifts for each DL specified. This is done by numerically integrating to obtain DL(z) up to the maximum required DL and then interplating to obtain z(DL)
+        return self.c_over_Ho/self.z2E(z)
+
+    def DL2z(self, DL, dz=DEFAULT_DZ):
+        """returns redshifts for each DL specified. This is done by numerically integrating to obtain DL(z) up to the maximum required DL and then interplating to obtain z(DL)
         """
         max_DL = np.max(DL)
+        if max_DL > np.max(self._distances['DL']): ### need to extend the integration
+            self._extend_DL(max_DL)
 
-        raise NotImplementedError('implement numeric integration')
+        return np.interpolate(DL, self._distances['DL'], self._distances['z'])
+
+    def z2DL(self, z, dz=DEFAULT_DZ):
+        """returns luminosity distance at the specified redshifts
+        """
+        max_z = np.max(z)
+        while max_z > np.max(self._distances['z']):
+            self._extend_DL(2*np.max(self._distances['DL']), dz=dz) ### double the luminosity distance
+
+        return np.interpolate(z, self._distances['z'], self._distances['DL'])
+
+    def _extend_DL(self, max_DL):
+        """integrates out distances until we hit max_DL
+        """
+        z_list = list(self._distances['z'])
+        DL_list = list(self._distances['DL'])
+
+        current_DL = DL_list[-1]
+        current_z = z_list[-1]
+
+        # initialize integration
+        current_dDLdz = self.dDLdz(current_z)
+
+        # iterate until we are far enough
+        while current_DL < max_DL:
+            current_z += dz                                ### increment
+            dDLdz = self.dDLdz(current_z)                  ### evaluated at the next step
+            current_DL += 0.5*(current_dDLdz + dDLdz) * dz ### trapazoidal approximation
+            current_dDLdz = dDLdz                          ### update
+
+            DL_list.append(current_DL)                     ### append
+            z_list.append(current_z)
+
+        self._distances['z'] = np.array(z_list, dtype=float)
+        self._distances['DL'] = np.array(DL_list, dtype=float)
 
 #-------------------------------------------------
 
 ### Planck 2018 Cosmology (Table1 in arXiv:1807.06209)
-PLANCK_2018_Ho = 67.32
+PLANCK_2018_Ho = 67.32 * (3.216156*1e6)**-1 * (utils.c*86400*365)**-1 * (1e5) ### km/s/Mpc * (Mpc/lyr) * (lyr/cm) * (cm/km) = s**-1
 PLANCK_2018_OmegaMatter = 0.3158
-PLANCK_2018_OmegaLambda = 1.-PLANCK_2018_OmegaMatter
+PLANCK_2018_OmegaLambda = 1. - PLANCK_2018_OmegaMatter
 PLANCK_2018_OmegaRadiation = 0.
 PLANCK_2018_OmegaKappa = 0.
 
