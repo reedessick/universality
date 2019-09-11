6d3fa1ba5f05d0da46e0074312fa2208dbcb0efa
Reed Essick <reed.essick@ligo.org>
2019-09-11 11:52:03 -0700

    added Msun as a reference unit

--- universality/utils.py
+++ universality/utils.py
@@ -13,9 +13,10 @@ from . import stats
 
 #-------------------------------------------------
 
-G = 6.674e-8        # newton's constant in g^-1 cm^3 s^-2
+G = 6.674e-8        # newton's constant in (g^-1 cm^3 s^-2)
 c = (299792458*100) # speed of light in (cm/s)
 c2 = c**2
+Msun = 1.989e33     # mass of the sun in (g)
 
 #------------------------
 
