d0ae3aa168815261b395dd0064f8167336748ba3
Reed Essick <reed.essick@ligo.org>
2019-12-14 16:13:52 -0800

    pushed some code around and added a new executable

--- universality/utils.py
+++ universality/utils.py
@@ -3,6 +3,8 @@ __author__ = "reed.essick@ligo.org"
 
 #-------------------------------------------------
 
+import glob
+
 from collections import defaultdict
 import numpy as np
 
@@ -404,6 +406,114 @@ def logcumkde(samples, data, variance, bounds=None, weights=None, direction=DEFA
     """
     raise NotImplementedError('This is a non-trivial thing to do, and I have not done it yet')
 
+def process2samples(
+        data,
+        tmp,
+        mod,
+        xcolumn,
+        ycolumns,
+        x_test,
+        verbose=False,
+    ):
+    """manages I/O and extracts samples at the specified places
+    """
+    loadcolumns = [xcolumn] + ycolumns
+    Nref = len(x_test)
+
+    ans = np.empty((len(data), Nref*len(ycolumns)), dtype=float)
+    for i, eos in enumerate(data):
+        path = tmp%{'moddraw':eos//mod, 'draw':eos}
+        if verbose:
+            print('    '+path)
+        d, c = load(path, loadcolumns)
+
+        for j, column in enumerate(c[1:]):
+            ans[i,j*Nref:(j+1)*Nref] = np.interp(x_test, d[:,0], d[:,loadcolumns.index(column)])
+
+    return ans
+
+def process2quantiles(
+        data,
+        tmp,
+        mod,
+        xcolumn,
+        ycolumn,
+        x_test,
+        quantiles,
+        quantile_type='sym',
+        x_multiplier=1.,
+        y_multiplier=1.,
+        weights=None,
+        verbose=False,
+    ):
+    """manages I/O and extracts quantiles at the specified places
+    """
+    y_test = [] ### keep this as a list because we don't know how many stable branches there are
+    w_test = []
+    num_points = len(x_test)
+
+    truth = np.empty(num_points, dtype=bool) ### used to extract values
+
+    columns = [xcolumn, ycolumn]
+    if weights is None:
+        weights = np.ones(len(data), dtype=float) / len(data)
+
+    for eos, weight in zip(data, weights): ### iterate over samples and compute weighted moments
+        for eos_path in glob.glob(tmp%{'moddraw':eos//mod, 'draw':eos}):
+            if verbose:
+                print('    '+eos_path)
+            d, _ = load(eos_path, columns)
+
+            d[:,0] *= x_multiplier
+            d[:,1] *= y_multiplier
+
+            _y = np.empty(num_points, dtype=float)
+            _y[:] = np.nan ### signal that nothing was available at this x-value
+
+            truth[:] = (np.min(d[:,0])<=x_test)*(x_test<=np.max(d[:,0])) ### figure out which x-test values are contained in the data
+            _y[truth] = np.interp(x_test[truth], d[:,0], d[:,1]) ### fill those in with interpolated values
+
+            y_test.append( _y ) ### add to the total list
+            w_test.append( weight )
+
+    if len(y_test)==0:
+        raise RuntimeError('could not find any files matching "%s"'%temp)
+
+    y_test = np.array(y_test) ### cast to an array
+    w_test = np.array(w_test)
+
+    ### compute the quantiles
+    Nquantiles = len(quantiles)
+    if quantile_type=='hpd':
+        Nquantiles *= 2 ### need twice as many indicies for this
+
+    qs = np.empty((Nquantiles, num_points), dtype=float)
+    med = np.empty(num_points, dtype=float)
+
+    for i in xrange(num_points):
+
+        _y = y_test[:,i]
+        truth = _y==_y
+        _y = _y[truth] ### only keep things that are not nan
+        _w = w_test[truth]
+
+        if quantile_type=="sym":
+            qs[:,i] = stats.quantile(_y, quantiles, weights=_w)     ### compute quantiles
+
+        elif quantile_type=="hpd":
+
+            ### FIXME: the following returns bounds on a contiguous hpd interval, which is not necessarily what we want...
+
+            bounds = stats.samples2crbounds(_y, quantiles, weights=_w) ### get the bounds
+            qs[:,i] = np.array(bounds).flatten()
+
+        else:
+            raise ValueError('did not understand --quantile-type=%s'%quantile_type)
+
+        med[i] = stats.quantile(_y, [0.5], weights=_w)[0] ### compute median
+
+    return qs, med
+
 #-------------------------------------------------
 # KDE and cross-validation likelihood
 #-------------------------------------------------
@@ -755,7 +865,7 @@ def logleavekoutLikelihood(data, variances, k=1, weights=None, num_proc=DEFAULT_
         sets[i%Nsets].append(i)
 
     logL = np.empty(Nsets, dtype='float')
-    grad_logL = np.empty((Nsets, Ndim), dtype='float')
+#    grad_logL = np.empty((Nsets, Ndim), dtype='float')
 
     twov = -0.5/variances
     truth = np.ones(Nsamples, dtype=bool)
@@ -770,7 +880,7 @@ def logleavekoutLikelihood(data, variances, k=1, weights=None, num_proc=DEFAULT_
         logL[i] = np.sum(logkde(sample, data[truth], variances, weights=weights[truth], num_proc=num_proc))
 
         ### compute gradient of logLikelihood
-        grad_logL[i,:] = np.sum(grad_logkde(sample, data[truth], variances, weights=weights[truth], num_proc=num_proc), axis=0)
+#        grad_logL[i,:] = np.sum(grad_logkde(sample, data[truth], variances, weights=weights[truth], num_proc=num_proc), axis=0)
 
     ### add in constant terms to logL
     logL -= np.log(Nsets-1) ### take the average as needed
@@ -779,10 +889,11 @@ def logleavekoutLikelihood(data, variances, k=1, weights=None, num_proc=DEFAULT_
     mlogL = np.mean(weights*logL) # scalar
     vlogL = np.var(weights*logL)  # scalar
 
-    mglogL = np.mean(weights*grad_logL.transpose(), axis=1)  # vector: (Ndim,)
-    vglogL = np.cov(weights*grad_logL.transpose(), rowvar=1) # matrix: (Ndim, Ndim)
+#    mglogL = np.mean(weights*grad_logL.transpose(), axis=1)  # vector: (Ndim,)
+#    vglogL = np.cov(weights*grad_logL.transpose(), rowvar=1) # matrix: (Ndim, Ndim)
 
-    return mlogL, vlogL, mglogL, vglogL
+#    return mlogL, vlogL, mglogL, vglogL
+    return mlogL, vlogL, None, None
 
 #-------------------------------------------------
 
