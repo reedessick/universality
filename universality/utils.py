340a94f06bf9d5fb50841ccd23056ce9d573f39a
Reed Essick <reed.essick@ligo.org>
2020-07-22 08:38:21 -0700

    updated logic to identify stable branches to make sure we don't smooth over any small stable regions. Note, we rely on quite precise mass estiamtes from the TOV solver, which may be difficult/unreliable if it uses a termination condition based on the vanishing pressure at the stellar surface

--- universality/utils.py
+++ universality/utils.py
@@ -1033,31 +1033,63 @@ def Mrhoc2branches(M, rhoc):
     Note! assumes models are ordered by increasing rhoc
     returns a list of boolean arrays denoting where each branch starts and ends
     """
+    assert np.all(np.diff(rhoc)>0), 'rhoc must be ordered from smallest to largest!'
+
     N = len(M)
+    N1 = N - 1
+
     branches = []
 
-    # iterate over all data points, determining stability by numeric derivatives of dM/drhoc
-    stable = False
-    for i, dM_drhoc in enumerate(np.gradient(M, rhoc)):
-        if dM_drhoc > 0: ### stable branch
-            if stable:
-                branch.append(i)
-            else:
-                branch = [i]
-                stable = True
-        elif stable: ### was on a stable branch, and it just ended
-            stable = False
-            branches.append(_branch2bool(branch, N)) ### append
+    ### assume stellar models are ordered by increasing rhoc
+    ### we just check the differences between M
+    start = 0
+    end = 0
+    while end < N1:
+        if M[end+1] > M[end]:
+            end += 1
+        else:
+            if start!=end:
+                branches.append(_bounds2bool(start, end, N))
+            end += 1
+            start = end
 
-    if stable:
-        branches.append(_branch2bool(branch, N)) ### append to pick up what was left when we existed the loop
+    if start!=end:
+        branches.append(_bounds2bool(start, end, N))
 
     return branches
 
-def _branch2bool(branch, N):
+def _bounds2bool(start, end, N):
     ans = np.zeros(N, dtype=bool)
-    ans[branch] = True
+    ans[start:end+1] = True ### NOTE, this is inclusive
     return ans
+
+#def Mrhoc2branches(M, rhoc):
+#    """take the M-rhoc curve and separate it into separate stable branches.
+#    Note! assumes models are ordered by increasing rhoc
+#    returns a list of boolean arrays denoting where each branch starts and ends
+#    """
+#    # iterate over all data points, determining stability by numeric derivatives of dM/drhoc
+#    stable = False
+#    for i, dM_drhoc in enumerate(np.gradient(M, rhoc)):
+#        if dM_drhoc > 0: ### stable branch
+#            if stable:
+#                branch.append(i)
+#            else:
+#                branch = [i]
+#                stable = True
+#        elif stable: ### was on a stable branch, and it just ended
+#            stable = False
+#            branches.append(_branch2bool(branch, N)) ### append
+#
+#    if stable:
+#        branches.append(_branch2bool(branch, N)) ### append to pick up what was left when we existed the loop
+#
+#    return branches
+#
+#def _branch2bool(branch, N):
+#    ans = np.zeros(N, dtype=bool)
+#    ans[branch] = True
+#    return ans
 
 #-------------------------------------------------
 # utilities associated with integrating the EOS realizations
