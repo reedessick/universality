922f6514d0f6069ffafe0e3c384e4f8db1e7215b
Reed Essick <reed.essick@ligo.org>
2020-10-20 19:06:42 -0500

    added a few more units

--- universality/utils/units.py
+++ universality/utils/units.py
@@ -7,5 +7,10 @@ __author__ = "Reed Essick (reed.essick@gmail.com)"
 G = 6.674e-8        # newton's constant in (g^-1 cm^3 s^-2)
 c = (299792458*100) # speed of light in (cm/s)
 c2 = c**2
+
 Msun = 1.989e33     # mass of the sun in (g)
-Mnuc = 1.67262192370e-24 ### mass of the proton (g)
+
+Mproton = 1.67262192370e-24 ### mass of the proton (g)
+Mneutron = 1.67492749804e-14 ### mass of the neutron (g)
+
+Mnuc = Mproton ### an arbitrary choice for reference.
