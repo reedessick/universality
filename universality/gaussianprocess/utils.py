196c5edb9eecf7433fc3dddac6bcad348a743b05
Reed Essick <reed.essick@ligo.org>
2021-01-28 18:27:33 -0800

    added option to integrate-phi when drawing gpr realizations. This should simplify matters a bit, as we can now 'generate directories full of EoS' with a relatively simple command line

--- universality/gaussianprocess/utils.py
+++ universality/gaussianprocess/utils.py
@@ -10,10 +10,79 @@ from universality import eos
 from universality.utils import utils
 
 #-------------------------------------------------
+
+DEFAULT_SIGMA_LOGPRESSUREC2 = 0.0
+
+#-------------------------------------------------
 # utilities associated with integrating the EOS realizations
 #-------------------------------------------------
 
-def stitch_below_reference_pressure(energy_densityc2, pressurec2, reference_pressurec2):
+def integrate_phi(
+        pressurec2,
+        phi,
+        reference_pressurec2,
+        sigma_logpressurec2=DEFAULT_SIGMA_LOGPRESSUREC2,
+        stitch_below_reference_pressure=False,
+        include_baryon_density=True,
+        include_cs2c2=False,
+        include_baryon_chemical_potential=False,
+        verbose=False,
+    ):
+    '''numerically integrates the realization from a GP to get the EoS
+    '''
+    denergy_densitydpressure = (1 + np.exp(phi)) ### this is the definition of phi=log(de/dp - 1)
+
+    # perform numeric integration with trapazoidal approximation
+    # NOTE: this could probably be improved...
+    if verbose:
+        print('performing numeric integration for energy_density via trapazoidal approximation')
+    reference_pressurec2 = np.exp(np.log(reference_pressurec2) + np.random.randn()*sigma_logpressurec2)
+    energy_densityc2 = dedp2e(denergy_densitydpressure, pressurec2, reference_pressurec2)
+    if stitch_below_reference_pressure:
+        energy_densityc2, pressurec2 = stitch_below_pressure(energy_densityc2, pressurec2, reference_pressurec2)
+
+    ### throw away pressures corresponding to negative energy densities!
+    truth = energy_densityc2 >= 0
+
+    pressurec2 = pressurec2[truth]
+    energy_densityc2 = energy_densityc2[truth]
+
+    data = [pressurec2, energy_densityc2]
+    cols = ['pressurec2', 'energy_densityc2']
+
+    #------------------------
+
+    # perform numeric integration to obtain baryon density along with energy_density and pressure
+    if include_baryon_density:
+        if verbose:
+            print('performing numeric integration for baryon_density via trapazoidal approximation')
+        baryon_density = e_p2rho(energy_densityc2, pressurec2, reference_pressurec2, verbose=verbose)
+        if stitch_below_reference_pressure:
+            ### the following indecies should match up because of the call to stitch_below_pressure made above
+            baryon_density[pressurec2<=reference_pressurec2] = eos.CRUST_BARYON_DENSITY[eos.CRUST_PRESSUREC2<=reference_pressurec2]
+
+        data.append(baryon_density)
+        cols.append('baryon_density')
+
+    # take numeric derivative to estimate sound speed
+    if include_cs2c2:
+        if verbose:
+            print('computing cs2c2 via numeric differentiation')
+        data.append(utils.num_dfdx(energy_densityc2, pressurec2))
+        cols.append('cs2c2')
+
+    # take numeric derivative to estimate chemical potential
+    if include_baryon_chemical_potential:
+        if verbose:
+            print('computing baryon_chemical_potential via numeric differentiation')
+        data.append(utils.num_dfdx(baryon_density, energy_densityc2) * units.Mnuc)
+        cols.append('baryon_chemical_potential')
+
+    # return
+    data = np.transpose(np.array(data, dtype=float))
+    return data, cols
+
+def stitch_below_pressure(energy_densityc2, pressurec2, reference_pressurec2):
     """reutrn energy_densityc2, pressurec2"""
     crust_truth = eos.CRUST_PRESSUREC2 < reference_pressurec2
     eos_truth = reference_pressurec2 <= pressurec2
