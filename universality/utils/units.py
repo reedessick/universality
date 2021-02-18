03e20508b386adcba9637036ef8b44e252b8ba9f
Reed Essick <reed.essick@ligo.org>
2021-02-17 19:56:37 -0800

    changed the value of the speed of light in vaccuum from an into to a float so we don't have to worry about type casting in numpy, which appears to be fragile in unexpected ways at times

--- universality/utils/units.py
+++ universality/utils/units.py
@@ -5,7 +5,7 @@ __author__ = "Reed Essick (reed.essick@gmail.com)"
 #-------------------------------------------------
 
 G = 6.674e-8        # newton's constant in (g^-1 cm^3 s^-2)
-c = (299792458*100) # speed of light in (cm/s)
+c = (299792458.0*100) # speed of light in (cm/s) NOTE, this really needs to be a float, not an int
 c2 = c**2
 
 Msun = 1.989e33     # mass of the sun in (g)
