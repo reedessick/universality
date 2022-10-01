bc315d468bb0263de0495c50a9535b96c42f03a5
Reed Essick <reed.essick@gmail.com>
2022-09-30 20:07:05 -0400

    began work to make extract-count and process2count smarter by counting the number of unique values in a column and supporting selection criteria on other columns. Selection criteria still need to be implemented and everything needs to be tested

--- universality/properties/count.py
+++ universality/properties/count.py
@@ -10,29 +10,64 @@ from universality.utils import io
 
 #-------------------------------------------------
 
-def data2count(d):
+def required_columns(reference, greater_than=[], less_than=[], overlaps=[]):
+    columns = [reference]
+    columns += [k for k, _ in greater_than + less_than]
+    for k, K, _, _ in overlaps:
+        columns += [k, K]
+
+#------------------------
+
+def data2count(data, cols, reference, greater_than=[], less_than=[], overlaps=[]):
     """so stupid it hardly merrits a delegation, but at least we have this defined in a single place
     """
-    return len(d)
+    truth = np.ones(len(data), type=bool)
 
-COUNT_TEMPLATE = 'num%s'
+    raise NotImplementedError('''\
+select which rows are valid based on greater_than, less_that, overlaps
+then count the number of unique values within cols.index(reference)
+''')
+
+    return len(np.unique(data[cols.index(reference)][truth]))
+
+#------------------------
+
+COUNT_TEMPLATE = 'num_%s'
 
 def process2count(
         data,
         tmp,
         mod,
         reference_column,
+        greater_than=[],
+        less_than=[],
+        overlaps=[],
         verbose=False,
     ):
     """handles I/O and extracts the number of lines in a particular file (like the number of branches, etc)
     """
     N = len(data)
     ans = np.empty(N, dtype=int)
+
+    columns = required_columns(
+        reference_column,
+        greater_than=greater_than,
+        less_than=less_than,
+        overlaps=overlaps,
+    )
+
     for i, eos in enumerate(data):
         path = tmp%{'moddraw':eos//mod, 'draw':eos}
         if verbose:
             print('    %d/%d %s'%(i+1, N, path))
-        d, _ = io.load(path, [reference_column])
-        ans[i] = data2count(d)
+        data, cols = io.load(path, columns)
+        ans[i] = data2count(
+            data,
+            cols,
+            reference_column,
+            greater_than=greater_than,
+            less_than=less_than,
+            overlaps=overlaps,
+        )
 
     return ans, COUNT_TEMPLATE%reference_column
