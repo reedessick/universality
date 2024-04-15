16b98feb32582ed8b33aad272d59bf804a280540
Reed Essick <reed.essick@gmail.com>
2024-04-15 18:08:09 -0400

    updated setup script to point at where pip installs code locally

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
-export PATH=$PWD/opt/bin:$PATH
+export PATH=$PWD/opt/local/bin:$PATH
 #export PYTHONPATH=$PWD/opt/lib/python2.7/site-packages:$PYTHONPATH
 #export PYTHONPATH=$PWD/opt/lib/python3.6/site-packages/:$PYTHONPATH
-export PYTHONPATH=$PWD/opt/lib/python3.8/site-packages:$PYTHONPATH
+#export PYTHONPATH=$PWD/opt/lib/python3.8/site-packages:$PYTHONPATH
+export PYTHONPATH=$PWD/opt/local/lib/python3.10/dist-packages/:$PYTHONPATH
