58372b2878e9241591cf81f16ed0d74e2fd3fccf
Reed Essick <reed.essick@gmail.com>
2024-05-31 09:37:32 -0400

    fixed a typo

--- universality/utils/io.py
+++ universality/utils/io.py
@@ -112,7 +112,7 @@ def write(path, data, cols, footer='', tmpdir='.', tmpdigits=6, verbose=False):
     tmppath = os.path.join(tmpdir, "."+os.path.basename(path)+'-'+''.join(np.random.choice(ALPHANUMERIC, size=tmpdigits))) ### save to a temporary file
     if verbose:
         print('writing to temporary file location: '+tmppath)
-    np.savetxt(tmppath, data, header=','.join(cols), comments='', delimiter=',')
+    np.savetxt(tmppath, data, header=','.join(cols), footer=footer, comments='', delimiter=',')
 
     if verbose:
         print('moving to final location: '+path)
